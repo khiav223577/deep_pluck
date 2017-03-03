32ceb3c05c9c0d2a38a3b9b313d86a785deb20f1
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2017-03-03 19:00:16 +0800

    test 2 level deep pluck

--- test/deep_pluck_test.rb
+++ test/deep_pluck_test.rb
@@ -9,10 +9,17 @@ class DeepPluckTest < Minitest::Test
     refute_nil ::DeepPluck::VERSION
   end
   
-  def test_basic_pluck
+  def test_pluck_with_1_level_deep
     assert_equal [
       {'name' => 'John'},
-    ], User.where(:name => 'John').deep_pluck(:name)
+      {'name' => 'Pearl'},
+    ], User.where(:name => %w(John Pearl)).deep_pluck(:name)
   end
 
+  def test_pluck_with_2_level_deep
+    assert_equal [
+      {'name' => 'John'},
+      {'name' => 'Pearl'},
+    ], User.where(:name => %w(John Pearl)).includes(:posts).deep_pluck(:name, :posts => [:name])
+  end
 end
