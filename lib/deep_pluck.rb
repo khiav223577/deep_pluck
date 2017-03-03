2b9f993991f7cd63d397c2a382c4ff03725f131d
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2017-03-03 18:51:04 +0800

    add dependency

--- lib/deep_pluck.rb
+++ lib/deep_pluck.rb
@@ -1,2 +1,15 @@
 require "deep_pluck/version"
 require 'active_record'
+require 'pluck_all'
+
+class ActiveRecord::Relation
+	def deep_pluck(*args)
+		pluck_all(*args)
+	end
+end
+
+class ActiveRecord::Base
+  def self.deep_pluck(*args)
+    self.where('').deep_pluck(*args)
+  end
+end
--- test/deep_pluck_test.rb
+++ test/deep_pluck_test.rb
@@ -4,8 +4,15 @@ class DeepPluckTest < Minitest::Test
   def setup
     
   end
+  
   def test_that_it_has_a_version_number
     refute_nil ::DeepPluck::VERSION
   end
   
+  def test_basic_pluck
+    assert_equal [
+      {'name' => 'John'},
+    ], User.where(:name => 'John').deep_pluck(:name)
+  end
+
 end
