587e816d54308bd89f7b5cd36f693979f6f7937f
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2017-03-28 01:08:17 +0800

    fix rails 3 condition

--- test/seeds.rb
+++ test/seeds.rb
@@ -81,7 +81,11 @@ end
 class Achievement < ActiveRecord::Base
   has_many :user_achievements
   has_many :users, :through => :user_achievements
-  has_many :female_users, ->{ where(:gender => 'female')}, :through => :user_achievements, :foreign_key => "user_id", :source => :user
+  if Gem::Version.new(ActiveRecord::VERSION::STRING) < Gem::Version.new('4.0.0')
+    has_many :female_users, :conditions => {:gender => 'female'}, :through => :user_achievements, :foreign_key => "user_id", :source => :user
+  else
+    has_many :female_users, ->{ where(:gender => 'female')}, :through => :user_achievements, :foreign_key => "user_id", :source => :user
+  end
   has_and_belongs_to_many :users2, class_name: 'User', :join_table => :user_achievements
 end
 
