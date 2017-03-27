c022f09d8b5f5ac464e06f6358a146bde8b3227e
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2017-03-28 02:41:33 +0800

    move some methods to data_combiner

---
+++ lib/deep_pluck/data_combiner.rb
@@ -0,0 +1,37 @@
+module DeepPluck
+  module DataCombiner
+    class << self
+      def combine_data(parent, children, primary_key, column_name, foreign_key, reverse, collection)
+        source =  reverse ? parent : children
+        target = !reverse ? parent : children
+        data_hash = make_data_hash(collection, source, primary_key, column_name)
+        assign_values_to_parent(collection, target, data_hash, column_name, foreign_key, reverse: reverse)
+        return children
+      end
+    private
+      def make_data_hash(collection, parent, primary_key, column_name)
+        return parent.map{|s| [s[primary_key], s]}.to_h if !collection
+        hash = {}
+        parent.each do |model_hash|
+          key = model_hash[primary_key]
+          array = (hash[key] ? hash[key][column_name] : []) #share the children if id is duplicated
+          model_hash[column_name] = array
+          hash[key] = model_hash
+        end
+        return hash
+      end
+      def assign_values_to_parent(collection, parent, children_hash, column_name, foreign_key, reverse: false)
+        parent.each{|s|
+          next if (id = s[foreign_key]) == nil
+          left   =  reverse ? children_hash[id] : s
+          right  = !reverse ? children_hash[id] : s
+          if collection
+            left[column_name] << right
+          else
+            left[column_name] = right
+          end
+        }
+      end
+    end
+  end
+end
--- lib/deep_pluck/model.rb
+++ lib/deep_pluck/model.rb
@@ -1,3 +1,4 @@
+require 'deep_pluck/data_combiner'
 module DeepPluck
   class Model
   #---------------------------------------
@@ -79,42 +80,15 @@ module DeepPluck
       relation = with_conditions(reflect, relation)
       return relation.joins(get_join_table(reflect)).where(relation_key => ids)
     end
-    def make_data_hash(collection, parent, primary_key, column_name)
-      return parent.map{|s| [s[primary_key], s]}.to_h if !collection
-      hash = {}
-      parent.each do |model_hash|
-        key = model_hash[primary_key]
-        array = (hash[key] ? hash[key][column_name] : []) #share the children if id is duplicated
-        model_hash[column_name] = array
-        hash[key] = model_hash
-      end
-      return hash
-    end
-    def assign_values_to_parent(collection, parent, children_hash, column_name, foreign_key, reverse: false)
-      parent.each{|s|
-        next if (id = s[foreign_key]) == nil
-        left   =  reverse ? children_hash[id] : s
-        right  = !reverse ? children_hash[id] : s
-        if collection
-          left[column_name] << right
-        else
-          left[column_name] = right
-        end
-      }
-    end
     def set_includes_data(parent, column_name, model)
       reflect = get_reflect(column_name)
+      reverse = !reflect.belongs_to?
+      foreign_key = get_foreign_key(reflect, reverse: reverse)
       primary_key = get_primary_key(reflect)
       children = model.load_data{|relation| do_query(parent, reflect, relation) }
       #reverse = false: Child.where(:id => parent.pluck(:child_id))
       #reverse = true : Child.where(:parent_id => parent.pluck(:id))
-      reverse = !reflect.belongs_to?
-      source =  reverse ? parent : children
-      target = !reverse ? parent : children
-      foreign_key = get_foreign_key(reflect, reverse: reverse)
-      data_hash = make_data_hash(reflect.collection?, source, primary_key, column_name)
-      assign_values_to_parent(reflect.collection?, target, data_hash, column_name, foreign_key, reverse: reverse)
-      return children
+      return DataCombiner.combine_data(parent, children, primary_key, column_name, foreign_key, reverse, reflect.collection?)
     end
   public
     def load_data
