1ed5a7923d7f2ba19f8cf10fa133565aa2d920fd
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2017-03-27 17:29:46 +0800

    rails 3 condition

--- test/seeds.rb
+++ test/seeds.rb
@@ -44,7 +44,11 @@ end
 class User < ActiveRecord::Base
   serialize :serialized_attribute, Hash
   has_many :posts
-  has_many :posts_1_3, -> { where('title LIKE ? OR title LIKE ? ', '%post1', '%post3') }, :class_name => "Post"
+  if Gem::Version.new(ActiveRecord::VERSION::STRING) < Gem::Version.new('4.0.0')
+    has_many :posts_1_3, :conditions => ['title LIKE ? OR title LIKE ? ', '%post1', '%post3'], :class_name => "Post"
+  else
+    has_many :posts_1_3, -> { where('title LIKE ? OR title LIKE ? ', '%post1', '%post3') }, :class_name => "Post"
+  end
   has_one :contact
   has_one :contact2, :foreign_key => :user_id2
   has_many :user_achievements
