e1fae2104225176eb312e765b22aec302f1ce029
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2019-09-25 11:37:43 +0800

    test query num to make sure no extra query is fired

--- test/deep_pluck_at_model_test.rb
+++ test/deep_pluck_at_model_test.rb
@@ -2,18 +2,29 @@ require 'test_helper'
 
 class DeepPluckAtModelTest < Minitest::Test
   def test_1_level_deep
+    user = User.where(name: 'Pearl').first
     expected = { 'name' => 'Pearl' }
-    assert_equal(expected, User.where(name: %w[Pearl]).first.deep_pluck(:name))
+
+    assert_queries(0) do
+      assert_equal(expected, user.deep_pluck(:name))
+    end
   end
 
   def test_2_level_deep
+    user = User.where(name: 'Pearl').first
     expected = { 'name' => 'Pearl', :posts => [{ 'name' => 'post4' }, { 'name' => 'post5' }] }
-    assert_equal(expected, User.where(name: %w[Pearl]).first.deep_pluck(:name, posts: [:name]))
+
+    assert_queries(1) do
+      assert_equal(expected, user.deep_pluck(:name, posts: [:name]))
+    end
   end
 
   def test_2_level_deep_with_id
     user = User.where(name: 'Pearl').first
     expected = { 'id' => user.id, 'name' => 'Pearl', :posts => [{ 'name' => 'post4' }, { 'name' => 'post5' }] }
-    assert_equal(expected, user.deep_pluck(:id, :name, posts: :name))
+
+    assert_queries(1) do
+      assert_equal(expected, user.deep_pluck(:id, :name, posts: :name))
+    end
   end
 end
