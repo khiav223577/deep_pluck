6cfb1cd7cb26c9e406a0a33e51146a7a1f71906b
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2021-04-10 15:50:19 +0800

    add test cases

--- test/deep_pluck_has_and_belongs_to_many_test.rb
+++ test/deep_pluck_has_and_belongs_to_many_test.rb
@@ -4,10 +4,26 @@ class DeepPluckHasAndBelongsToManyTest < Minitest::Test
   def setup
   end
 
-  def test_has_and_belongs_to_many
+  def test_simple_case
     assert_equal [
       { zipcodes: [{ 'city' => 'Atlanta' }, { 'city'=>'Union City' }] },
       { zipcodes: [{ 'city' => 'Minneapolis' }, { 'city'=>'Edina' }] },
     ], County.deep_pluck(zipcodes: :city)
+  end
+
+  def test_custom_association_name
+    assert_equal TrainingProvider.deep_pluck(:name, borrower_training_programs: :name), [
+      'name' => 'provider X',
+      borrower_training_programs: [
+        'name' => 'program A',
+      ],
+    ]
+
+    assert_equal TrainingProgram.deep_pluck(:name, training_providers: :name), [
+      'name' => 'program A',
+      training_providers: [
+        'name' => 'provider X',
+      ],
+    ]
   end
 end
