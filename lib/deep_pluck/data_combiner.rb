0f2117f54e25c5356251a72aa6e32e7b02abc39c
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2023-08-10 16:36:23 +0800

    set the default value of parent only if in reverse

--- lib/deep_pluck/data_combiner.rb
+++ lib/deep_pluck/data_combiner.rb
@@ -4,29 +4,29 @@ module DeepPluck
       def combine_data(parent, children, primary_key, column_name, foreign_key, reverse, collection)
         source =  reverse ? parent : children
         target = !reverse ? parent : children
-        data_hash = make_data_hash(collection, source, primary_key, column_name)
+        data_hash = make_data_hash(collection, source, primary_key, column_name, reverse)
         assign_values_to_parent(collection, target, data_hash, column_name, foreign_key, reverse: reverse)
         return children
       end
 
       private
 
-      def make_data_hash(collection, parent, primary_key, column_name)
+      def make_data_hash(collection, parent, primary_key, column_name, reverse)
         hash = {}
 
-        if collection
-          parent.each do |model_hash|
-            key = model_hash[primary_key]
-            array = (hash[key] ? hash[key][column_name] : []) # share the children if id is duplicated
-            model_hash[column_name] = array # set the value of this association key to empty array by default
-            hash[key] = model_hash
+        parent.each do |model_hash|
+          key = model_hash[primary_key]
+
+          if reverse
+            if collection
+              array = (hash[key] ? hash[key][column_name] : []) # share the children if id is duplicated
+              model_hash[column_name] = array # set the value of this association key to empty array by default
+            else
+              model_hash[column_name] = nil # set the value of this association key to be nil by default
+            end
           end
-        else
-          parent.each do |model_hash|
-            key = model_hash[primary_key]
-            model_hash[column_name] ||= nil # set the value of this association key to be nil by default
-            hash[key] = model_hash
-          end
+
+          hash[key] = model_hash
         end
 
         return hash
