8d33401a59928e68abfd3993e4dea98b56357693
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2023-08-26 19:13:55 +0800

    Fix missing data caused by wrong association definition in test cases

--- test/deep_pluck_primary_key_test.rb
+++ test/deep_pluck_primary_key_test.rb
@@ -9,7 +9,7 @@ class DeepPluckPrimaryKeyTest < Minitest::Test
   def test_species
     assert_equal [
       { 'name' => 'John', 'species' => [{ 'name' => 'Bat' }, { 'name' => 'bat' }, { 'name' => 'batmen' }] },
-      { 'name' => 'Pearl', 'species' => [] },
+      { 'name' => 'Pearl', 'species' => [{ 'name' => 'Bat' }, { 'name' => 'bat' }, { 'name' => 'batmen' }] },
       { 'name' => 'Doggy', 'species' => [{ 'name' => 'Rat' }, { 'name' => 'rat' }] },
     ], User.where(name: %w[John Pearl Doggy]).deep_pluck(:name, 'species' => :name)
   end
